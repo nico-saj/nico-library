36f1178344a767ceb0047237eaaeb1444ba5d589
Mykola Saienko <nicolas.ozz.com@gmail.com>
2021-08-10 10:41:57 +0300

    Set partition

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,20 +24,23 @@ Item.create!(
       title: "Martian Chronicles",
       description: "Cult book by Ray Bradbury",
       user: john,
-      image_url: "https://upload.wikimedia.org/wikipedia/en/4/45/The-Martian-Chronicles.jpg"
+      image_url: "https://upload.wikimedia.org/wikipedia/en/4/45/The-Martian-Chronicles.jpg",
+      created_at: 2.months.ago
     },
     {
       title: "The Martian",
       description: "Novel by Andy Weir about an astronaut stranded on Mars trying to survive",
       user: john,
-      image_url: "https://upload.wikimedia.org/wikipedia/en/c/c3/The_Martian_2014.jpg"
+      image_url: "https://upload.wikimedia.org/wikipedia/en/c/c3/The_Martian_2014.jpg",
+      created_at: 5.months.ago
     },
     {
       title: "Doom",
       description: "A group of Marines is sent to the red planet via an ancient " \
                    "Martian portal called the Ark to deal with an outbreak of a mutagenic virus",
       user: jane,
-      image_url: "https://upload.wikimedia.org/wikipedia/en/5/57/Doom_cover_art.jpg"
+      image_url: "https://upload.wikimedia.org/wikipedia/en/5/57/Doom_cover_art.jpg",
+      created_at: 6.months.ago
     },
     {
       title: "Mars Attacks!",
